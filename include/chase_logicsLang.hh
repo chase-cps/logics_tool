477b474c6c695f13ad47deb207162c33035ea259
Michele Lora <michele.lora@univr.it>
2022-06-05 17:58:25 -0700

    Add library compilation and headers to installation procedure.

---
+++ include/chase_logicsLang.hh
@@ -0,0 +1,17 @@
+/**
+ * @author      <a href="mailto:michele.lora@univr.it">Michele Lora</a>
+ * @date        6/05/2022
+ * @brief       Main include file for the LogicsLang library of CHASE.
+ *              This project is released under the 3-Clause BSD License.
+ *
+ * */
+
+#pragma once
+
+#include "Console.hh"
+#include "LogicsSpecsBuilder.hh"
+
+#include "LogicsContractsBasedVisitor.h"
+#include "LogicsContractsLexer.h"
+#include "LogicsContractsParser.h"
+#include "LogicsContractsVisitor.h"
